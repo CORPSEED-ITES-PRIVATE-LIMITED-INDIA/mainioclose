389ee091301bb63a6831b2e3a0aa3e215ed4df52
avnishkumarcorp <avnish.kumar@corpseed.com>
2025-05-12 18:36:22 +0530

    ck editor implementation is done

---
+++ src/Main/Common/TextEditor.jsx
@@ -0,0 +1,325 @@
+/**
+ * This configuration was generated using the CKEditor 5 Builder. You can modify it anytime using this link:
+ * https://ckeditor.com/ckeditor-5/builder/#installation/NoNgNARATAdArDADBSAWAzOxAOKduoCccAjCelCNoiHKQYsYdiKiXIVESChAKYA7FIjDASYESPFSAupDgBDQn1qEIMoA
+ */
+
+import { useState, useEffect, useRef, useMemo } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Autoformat,
+  AutoImage,
+  Autosave,
+  BlockQuote,
+  Bold,
+  CKBox,
+  CKBoxImageEdit,
+  CloudServices,
+  Emoji,
+  Essentials,
+  Heading,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsert,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  MediaEmbed,
+  Mention,
+  Paragraph,
+  PasteFromOffice,
+  PictureEditing,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  Underline,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
+
+const LICENSE_KEY =
+  "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDgzMDM5OTksImp0aSI6IjJiNWI2N2YwLWQzNDctNDA1NS05ODliLWZjMWQ5NWFmZGQwYSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImQwNTg2MzU3In0.VrY1Ta3tFhPK33RVLUBbmSa_7-Gq81gmUkd4rD7wMfl4ChNiVTWJZGvtH6i1DKfXQFbz_epwHd3X7GC89VPWdw";
+
+const CLOUD_SERVICES_TOKEN_URL =
+  "https://f_v3z04zplsu.cke-cs.com/token/dev/885d2f4f11db923f216e23b79e86a0c206ba5d56713a31fcc0cc2047162c?limit=10";
+
+export default function TextEditor() {
+  const editorContainerRef = useRef(null);
+  const editorRef = useRef(null);
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+
+  useEffect(() => {
+    setIsLayoutReady(true);
+
+    return () => setIsLayoutReady(false);
+  }, []);
+
+  const { editorConfig } = useMemo(() => {
+    if (!isLayoutReady) {
+      return {};
+    }
+
+    return {
+      editorConfig: {
+        toolbar: {
+          items: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "|",
+            "emoji",
+            "link",
+            "insertImage",
+            "ckbox",
+            "mediaEmbed",
+            "insertTable",
+            "blockQuote",
+            "|",
+            "bulletedList",
+            "numberedList",
+            "todoList",
+            "outdent",
+            "indent",
+          ],
+          shouldNotGroupWhenFull: false,
+        },
+        plugins: [
+          Autoformat,
+          AutoImage,
+          Autosave,
+          BlockQuote,
+          Bold,
+          CKBox,
+          CKBoxImageEdit,
+          CloudServices,
+          Emoji,
+          Essentials,
+          Heading,
+          ImageBlock,
+          ImageCaption,
+          ImageInline,
+          ImageInsert,
+          ImageInsertViaUrl,
+          ImageResize,
+          ImageStyle,
+          ImageTextAlternative,
+          ImageToolbar,
+          ImageUpload,
+          Indent,
+          IndentBlock,
+          Italic,
+          Link,
+          LinkImage,
+          List,
+          ListProperties,
+          MediaEmbed,
+          Mention,
+          Paragraph,
+          PasteFromOffice,
+          PictureEditing,
+          Table,
+          TableCaption,
+          TableCellProperties,
+          TableColumnResize,
+          TableProperties,
+          TableToolbar,
+          TextTransformation,
+          TodoList,
+          Underline,
+        ],
+        cloudServices: {
+          tokenUrl: CLOUD_SERVICES_TOKEN_URL,
+        },
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              title: "Paragraph",
+              class: "ck-heading_paragraph",
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "Heading 1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "Heading 2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "Heading 3",
+              class: "ck-heading_heading3",
+            },
+            {
+              model: "heading4",
+              view: "h4",
+              title: "Heading 4",
+              class: "ck-heading_heading4",
+            },
+            {
+              model: "heading5",
+              view: "h5",
+              title: "Heading 5",
+              class: "ck-heading_heading5",
+            },
+            {
+              model: "heading6",
+              view: "h6",
+              title: "Heading 6",
+              class: "ck-heading_heading6",
+            },
+          ],
+        },
+        image: {
+          toolbar: [
+            "toggleImageCaption",
+            "imageTextAlternative",
+            "|",
+            "imageStyle:inline",
+            "imageStyle:wrapText",
+            "imageStyle:breakText",
+            "|",
+            "resizeImage",
+            "|",
+            "ckboxImageEdit",
+          ],
+        },
+        initialData: "",
+        licenseKey: LICENSE_KEY,
+        link: {
+          addTargetToExternalLinks: true,
+          defaultProtocol: "https://",
+          decorators: {
+            toggleDownloadable: {
+              mode: "manual",
+              label: "Downloadable",
+              attributes: {
+                download: "file",
+              },
+            },
+          },
+        },
+        list: {
+          properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true,
+          },
+        },
+        mention: {
+          feeds: [
+            {
+              marker: "@",
+              feed: [
+                /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+              ],
+            },
+          ],
+        },
+        placeholder: "Type or paste your content here!",
+        table: {
+          contentToolbar: [
+            "tableColumn",
+            "tableRow",
+            "mergeTableCells",
+            "tableProperties",
+            "tableCellProperties",
+          ],
+        },
+      },
+    };
+  }, [isLayoutReady]);
+
+  useEffect(() => {
+    if (editorConfig) {
+      configUpdateAlert(editorConfig);
+    }
+  }, [editorConfig]);
+
+  return (
+    <div className="main-container">
+      <div
+        className="editor-container editor-container_classic-editor"
+        ref={editorContainerRef}
+        style={{ width: "100%" }}
+      >
+        <div className="editor-container__editor">
+          <div ref={editorRef}>
+            {editorConfig && (
+              <CKEditor editor={ClassicEditor} config={editorConfig} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * This function exists to remind you to update the config needed for premium features.
+ * The function can be safely removed. Make sure to also remove call to this function when doing so.
+ */
+function configUpdateAlert(config) {
+  if (configUpdateAlert.configUpdateAlertShown) {
+    return;
+  }
+
+  const isModifiedByUser = (currentValue, forbiddenValue) => {
+    if (currentValue === forbiddenValue) {
+      return false;
+    }
+
+    if (currentValue === undefined) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const valuesToUpdate = [];
+
+  configUpdateAlert.configUpdateAlertShown = true;
+
+  if (
+    !isModifiedByUser(
+      config.cloudServices?.tokenUrl,
+      "<YOUR_CLOUD_SERVICES_TOKEN_URL>"
+    )
+  ) {
+    valuesToUpdate.push("CLOUD_SERVICES_TOKEN_URL");
+  }
+
+  if (valuesToUpdate.length) {
+    window.alert(
+      [
+        "Please update the following values in your editor config",
+        "to receive full access to Premium Features:",
+        "",
+        ...valuesToUpdate.map((value) => ` - ${value}`),
+      ].join("\n")
+    );
+  }
+}
--- src/Main/Sales/Leads/Proposal.jsx
+++ src/Main/Sales/Leads/Proposal.jsx
@@ -1,47 +1,12 @@
-import React from "react";
-import { CKEditor, useCKEditorCloud } from "@ckeditor/ckeditor5-react";
+import React from 'react'
+import TextEditor from '../../Common/TextEditor'
 
-const Proposal = ({ leadid }) => {
-
-  const cloud = useCKEditorCloud({
-    version: "44.2.1",
-    premium: true,
-  });
-
-  if (cloud.status === "error") {
-    return <div>Error!</div>;
-  }
-
-  if (cloud.status === "loading") {
-    return <div>Loading...</div>;
-  }
-
-  const { ClassicEditor, Essentials, Paragraph, Bold, Italic,Alignment, } = cloud.CKEditor;
-
-  const { FormatPainter } = cloud.CKEditorPremiumFeatures;
-
+const Proposal = () => {
   return (
-    <div style={{ width: "80%", margin: "auto", padding: "20px" }}>
-      <h2>CKEditor Test</h2>
-      <CKEditor
-        editor={ClassicEditor}
-        data={"<p>Hello world!</p>"}
-        config={{
-          licenseKey: "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NDE5OTY3OTksImp0aSI6IjVkMGExNjI1LTlhYWUtNDUxMC04YmU4LWE1MDlhZDNiYmFlMyIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6ImI4MGYxMzEyIn0.I0zA81pk1-18TmJEqTgAMdNAGoxq1zDICCSiqzRFiNet6QiQrfuNPEQ16q7EDDvqc1ZPxUOCgLTDM6aVhFgMew",
-          plugins: [Essentials, Paragraph, Bold, Italic,Alignment, FormatPainter],
-          toolbar: [
-            "undo",
-            "redo",
-            "|",
-            "bold",
-            "italic",
-            "|",
-            "formatPainter",
-          ],
-        }}
-      />
+    <div>
+      <TextEditor/>
     </div>
-  );
-};
+  )
+}
 
-export default Proposal;
+export default Proposal
